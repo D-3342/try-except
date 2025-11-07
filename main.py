42b692f0d20b195d151d7eb0cf964054f0f9f9e6
D-3342 <Student@305-12.spobip-bel.ru>
2025-11-07 15:23:37 +0300

    hfgd

--- main.py
+++ main.py
@@ -32,27 +32,86 @@
 #     print("IndexError")
 
 # №5
-def math_operation():
-    num1 = input("Введите первое число: ")
-    num2 = input("Введите второе число: ")
-    operation = input("Введите операцию (+, -, *, /): ")
-    try:
-        num1 = float(num1)
-        num2 = float(num2)
-        if operation == '+':
-            result = num1 + num2
-        elif operation == '-':
-            result = num1 - num2
-        elif operation == '*':
-            result = num1 * num2
-        elif operation == '/':
-            try:
-                result = num1 / num2
-            except ZeroDivisionError:
-                print("Ошибка: деление на ноль невозможно.")
-                return
-        else:
-            raise ValueError("Неверная операция")
-        print(f"Результат {num1} {operation} {num2}: {result}")
-    except ValueError as e:
-        print(e)
+# def math_operation():
+#     num1 = input("Введите первое число: ")
+#     num2 = input("Введите второе число: ")
+#     operation = input("Введите операцию (+, -, *, /): ")
+#     try:
+#         num1 = float(num1)
+#         num2 = float(num2)
+#         if operation == '+':
+#             result = num1 + num2
+#         elif operation == '-':
+#             result = num1 - num2
+#         elif operation == '*':
+#             result = num1 * num2
+#         elif operation == '/':
+#             try:
+#                 result = num1 / num2
+#             except ZeroDivisionError:
+#                 print("Ошибка: деление на ноль невозможно.")
+#                 return
+#         else:
+#             raise ValueError("Неверная операция")
+#         print(f"Результат {num1} {operation} {num2}: {result}")
+#     except ValueError as e:
+#         print(e)
+# math_operation()
+
+# №6
+# while True:
+#     user_input = input("Введите число с плавающей точкой: ")
+#
+#     try:
+#         number = float(user_input)
+#         print(f"Введено число: {number}")
+#         break
+#     except ValueError:
+#         print("Ошибка ввода! Повторите попытку.")
+
+# №7
+# my_dict = {"password": 992911, "fox": 22, "cherry": 88}
+#
+# key = input("Введите ключ для доступа к значению: ")
+#
+# try:
+#     value = my_dict[key]
+#     print(f"Значение по ключу '{key}' равно {value}.")
+# except KeyError:
+#     print(f"Ключ '{key}' отсутствует в словаре")
+
+# №8
+# a = float(input("Введите первое число: "))
+# b = float(input("Введите второе число: "))
+# try:
+#     result = a / b
+#     print(f"{a} ÷ {b} = {result}")
+# except ZeroDivisionError:
+#     print("Деление на ноль невозможно!")
+# except TypeError:
+#     print("Аргументы должны быть числами.")
+
+# №9
+# file_name = input("Введите имя файла: ")
+# num_str = input("Введите число: ")
+#
+# try:
+#     with open(file_name, 'r') as file:
+#         content = file.read()
+#         num_from_file = float(content.strip())
+#     print(f"Число из файла: {num_from_file}, ваше число: {float(num_str)}")
+# except FileNotFoundError:
+#     print("Файл не найден!")
+# except ValueError:
+#     print("Ошибка формата числа в файле или вашем вводе!")
+
+# №10
+# try:
+#     num = float(input("Введите число: "))
+#     result = 100 / num
+#     final_result = int(result)
+#     print(f"Результат: {final_result}")
+# except ZeroDivisionError:
+#     print("Деление на ноль невозможно!")
+# except ValueError:
+#     print("Неверный формат числа, попробуйте ещё раз.")
